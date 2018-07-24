414d799a8b2b819785b8f341802423cf833ec498
Victor Khramov <vkhramov@lohika.com>
2018-07-24 15:51:24 +0300

    Initial commit - 8 parts ready

---
+++ ContosoUniversity/Migrations/20180716111507_FinalStudentPropertiesUpdate.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class FinalStudentPropertiesUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Student",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Student",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Student",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Student",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+        }
+    }
+}
